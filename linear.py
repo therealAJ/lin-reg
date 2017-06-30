6ab3d6746cf59c538a3819ea5acb1803c7757c7a
Alex Jordache <alexonej@gmail.com>
2017-06-30 10:32:34 -0400

    working model

--- linear.py
+++ linear.py
@@ -25,4 +25,34 @@ def cost_function(theta, X, y, m):
     cost = const * sse
     return cost
 
+def cfd(theta, X, y, sub_i, m):
+    sumErr = 0
+    for i in xrange(m):
+        x = X[i]
+        h_i = hypothesis(theta, x)
+        y_i = y[i]
+        err = (h_i - y_i) * x[sub_i]
+        sumErr += err
+    cost = (1 / m) * sumErr
+    return cost
+
+def gradient_descent(X, y, theta, m, alpha):
+    opt_theta = []
+    constant = alpha * (1 / m)
+    for i in xrange(len(theta)):
+        cost = cfd(theta, X, y, i, m)
+        updated_theta = theta[i] - constant * cost
+        opt_theta.extend(updated_theta)
+    return opt_theta
+
+def Linear_Regression(X, Y, alpha, theta, iter):
+    m = len(X)
+    for i in xrange(iter):
+        opt_theta = gradient_descent(X, y, theta, m, alpha)
+        theta = opt_theta
+    return opt_theta
+
+
+    
+
 
